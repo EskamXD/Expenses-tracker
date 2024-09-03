9b49f6070e4379839f3ddf602d3d9cd0ab1e76ef
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-09-03 11:28:20 +0200

    summary page work in progress, macos run shell script

--- expense_frontend/src/pages/SummaryPage.jsx
+++ expense_frontend/src/pages/SummaryPage.jsx
@@ -20,54 +20,27 @@ const SummaryPage = () => {
         direction: null,
         tab: null,
     });
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [selectedMonth, setSelectedMonth] = useState(
         new Date().getMonth() + 1
     ); // Domyślnie bieżący miesiąc
     const [selectedYear, setSelectedYear] = useState(new Date().getFullYear()); // Domyślnie bieżący rok
+    const [selectedSummary, setSelectedSummary] = useState("-");
+
+    const divToChange = document.getElementById("main");
 
     // Funkcja do pobierania wydatków z API
-    const fetchExpenses = async (month, year) => {
-        setLoading(true);
-        console.log("fetchExpenses", month, year);
+    const fetchExpenses = async (owner, month, year) => {
+        console.log("fetchExpenses", owner, month, year);
         try {
             const response = await axios.get(
                 `http://localhost:8000/api/expenses/?owner=${owner}&?month=${month}&?year=${year}`
             );
             const expenses = response.data;
-
-            // Filtracja wydatków
-            const kamil = expenses.filter(
-                (expense) => expense.owner === "kamil"
-            );
-            const ania = expenses.filter((expense) => expense.owner === "ania");
-            const wspolne = expenses.filter(
-                (expense) => expense.owner === "common"
-            );
-
-            // Ustawienie stanów dla wydatków
-            setKamilExpenses(kamil);
-            setAniaExpenses(ania);
-            setCommonExpenses(wspolne);
-
-            // Obliczanie sumy wszystkich wydatków
-            const total = expenses.reduce(
-                (sum, expense) => sum + parseFloat(expense.amount),
-                0
-            );
-            setTotalExpenses(total);
-
-            setLoading(false);
         } catch (error) {
             console.error("Error fetching expenses:", error);
-            setLoading(false);
         }
     };
-
-    // UseEffect do pobierania danych przy montowaniu komponentu
-    useEffect(() => {
-        fetchExpenses(selectedMonth, selectedYear);
-    }, []);
 
     const sortExpenses = (expenses, key, direction) => {
         return [...expenses].sort((a, b) => {
@@ -123,14 +96,24 @@ const SummaryPage = () => {
         return ""; // Brak strzałki, gdy nie sortowane
     };
 
-    useEffect(() => {
-        fetchExpenses(selectedMonth, selectedYear); // Pobierz dane dla bieżącego miesiąca na start
-    }, []);
-
     // Funkcja pomocnicza do uzyskania nazwy miesiąca
     const getMonthName = (monthNumber) => {
         const date = new Date(0, monthNumber - 1); // Tworzenie daty na podstawie numeru miesiąca
         return date.toLocaleString("default", { month: "long" }); // Użycie toLocaleString do uzyskania nazwy miesiąca
+    };
+
+    const formatTitle = (summary) => {
+        if (!summary) return "Wybierz podsumowanie"; // default title
+        return `${summary[0].toUpperCase() + summary.slice(1)}`;
+    };
+
+    const firstFetching = () => {
+        // clean divToChange
+        while (divToChange.firstChild) {
+            divToChange.removeChild(divToChange.firstChild);
+        }
+        setLoading(true);
+        fetchExpenses(selectedSummary, selectedMonth, selectedYear); // Pobierz dane dla bieżącego miesiąca na start
     };
 
     return (
@@ -139,7 +122,7 @@ const SummaryPage = () => {
                 <h1>Podsumowanie</h1>
                 <Tabs defaultActiveKey="expenses" id="summary-tabs">
                     <Tab eventKey="expenses" title="Wydatki">
-                        <Tabs
+                        {/* <Tabs
                             defaultActiveKey="kamil-expenses"
                             id="expenses-tabs">
                             <Tab
@@ -250,7 +233,49 @@ const SummaryPage = () => {
                                 <h3>Podsumowanie Wydatków</h3>
                                 <p>Całkowite wydatki: {totalExpenses} PLN</p>
                             </Tab>
-                        </Tabs>
+                            </Tabs> */}
+                        <div className="center-div" id="main">
+                            {loading ? (
+                                <Spinner animation="border" role="status">
+                                    <span className="sr-only"></span>
+                                </Spinner>
+                            ) : (
+                                <Row>
+                                    <DropdownButton
+                                        id="dropdown-basic-button-summary"
+                                        title={`Wybierz podsumowanie: ${formatTitle(
+                                            selectedSummary
+                                        )}`}
+                                        onSelect={(summary) => {
+                                            console.log(
+                                                "selectedSummary",
+                                                summary
+                                            );
+                                            setSelectedSummary(summary);
+                                            firstFetching();
+                                        }}>
+                                        <Dropdown.Item eventKey="-" key="-">
+                                            -
+                                        </Dropdown.Item>
+                                        <Dropdown.Item
+                                            eventKey="kamil"
+                                            key="kamil">
+                                            Kamil
+                                        </Dropdown.Item>
+                                        <Dropdown.Item
+                                            eventKey="ania"
+                                            key="ania">
+                                            Ania
+                                        </Dropdown.Item>
+                                        <Dropdown.Item
+                                            eventKey="Wspólne"
+                                            key="common">
+                                            Wspólne
+                                        </Dropdown.Item>
+                                    </DropdownButton>
+                                </Row>
+                            )}
+                        </div>
                     </Tab>
                     {/* Możesz dodać obsługę przychodów w podobny sposób */}
                     {/* <Tab eventKey="income" title="Przychody" className="p-3">
@@ -284,3 +309,4 @@ const SummaryPage = () => {
 };
 
 export default SummaryPage;
+
